342af70a921490b15461242e9b5203bd90561502
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-01 20:35:31 -0700

    Adding photo link to friends array and changing choices for all friends to have varying answers within friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,43 @@
 var friends = [
     {
         name: "Chrom",
-        photo: "TBD",
-        choice: [5, 2, 4, 1, 3, 4, 5, 3, 3, 2]
+        photo: "https://fireemblem.gamepress.gg/sites/fireemblem/files/2017-02/Full_Portrait_Chrom.png",
+        choice: [5, 3, 1, 1, 4, 4, 5, 5, 3, 5]
+    },
+    {
+        name: "Lucina",
+        photo: "https://fireemblem.gamepress.gg/sites/fireemblem/files/2017-01/Lucina.png",
+        choice: [5, 4, 1, 1, 5, 5, 3, 5, 1, 5]
+    },
+    {
+        name: "Kagero",
+        photo: "https://fireemblem.gamepress.gg/sites/fireemblem/files/2017-02/Full_Portrait_Kagero.png",
+        choice: [1, 2, 4, 1, 2, 4, 5, 3, 3, 3]
+    },
+    {
+        name: "Azura",
+        photo: "https://fireemblem.gamepress.gg/sites/fireemblem/files/2017-02/Full_Portrait_Azura.png",
+        choice: [3, 2, 2, 1, 4, 4, 3, 4, 3, 4]
+    },
+    {
+        name: "Fae",
+        photo: "https://fireemblem.gamepress.gg/sites/fireemblem/files/2017-02/Full_Portrait_Fae.png",
+        choice: [3, 2, 2, 4, 3, 4, 4, 3, 3, 5]
+    },
+    {
+        name: "Marth",
+        photo: "https://fireemblem.gamepress.gg/sites/fireemblem/files/2017-01/Marth_0.png",
+        choice: [4, 2, 1, 1, 3, 5, 5, 3, 3, 5]
+    },
+    {
+        name: "Lyn",
+        photo: "https://fireemblem.gamepress.gg/sites/fireemblem/files/2017-01/Lyn.png",
+        choice: [5, 2, 1, 1, 3, 4, 3, 3, 4, 4]
+    },
+    {
+        name: "Veronica",
+        photo: "https://fireemblem.gamepress.gg/sites/fireemblem/files/2017-06/Full_Portrait_Veronica.png",
+        choice: [2, 1, 5, 5, 1, 4, 3, 4, 2, 2]
     }
 ]
 
