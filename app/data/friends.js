453220cabc903cb63fc92adf5f6b25ae39dd39e1
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-01 18:11:34 -0700

    Adding module.exports = friends in order to export friends.js within friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,6 +2,8 @@ var friends = [
     {
         name: "Chrom",
         photo: "TBD",
-        choice: [5,2,4,1,3,4,5,3,3,2]
+        choice: [5, 2, 4, 1, 3, 4, 5, 3, 3, 2]
     }
 ]
+
+module.exports = friends;
